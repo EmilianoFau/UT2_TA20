828604753446e14f0182fa785b395af69ef16eea
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-08-20 23:47:19 -0300

    a

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiarImagen1() {
+    const imagen = document.getElementById('imagen');
+    imagen.src = 'luke.jpg';
+}
+
+function cambiarImagen2() {
+    const imagen = document.getElementById('imagen');
+    imagen.src = 'beer.jpg';
+}
